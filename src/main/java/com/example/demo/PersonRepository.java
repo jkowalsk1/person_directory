29d3b1b6724e94e46a33a2a94bdf04da091c6673
jkowalsk1 <jkowalsk@montgomerycollege.edu>
2017-08-09 19:32:41 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/PersonRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+/**
+ * Created by oracle on 8/7/17.
+ */
+
+import org.springframework.data.repository.CrudRepository;
+public interface PersonRepository extends CrudRepository<Person, Long>{
+
+}
